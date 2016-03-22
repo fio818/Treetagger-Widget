eae207bf237553338ebfc877a875d1b4d0dee625
mwuethr2 <michael.t.wuethrich@live.fr>
2016-03-22 08:56:03 +0100

    Added files via upload

---
+++ Help_open_widget.py
@@ -0,0 +1,36 @@
+Microsoft Windows [version 6.1.7601]
+Copyright (c) 2009 Microsoft Corporation. Tous droits réservés.
+
+C:\Users\mwuethr2>cd C:\Users\mwuethr2\Desktop\TreetaggerWidget\TreetaggerWidget
+
+
+C:\Users\mwuethr2\Desktop\TreetaggerWidget\TreetaggerWidget>python setup.py deve
+lop
+running develop
+running egg_info
+writing MyWidgets.egg-info\PKG-INFO
+writing top-level names to MyWidgets.egg-info\top_level.txt
+writing dependency_links to MyWidgets.egg-info\dependency_links.txt
+writing entry points to MyWidgets.egg-info\entry_points.txt
+reading manifest file 'MyWidgets.egg-info\SOURCES.txt'
+writing manifest file 'MyWidgets.egg-info\SOURCES.txt'
+running build_ext
+Creating c:\python27\arcgis10.2\lib\site-packages\MyWidgets.egg-link (link to .)
+
+MyWidgets 0.0.0 is already the active version in easy-install.pth
+
+Installed c:\users\mwuethr2\desktop\treetaggerwidget\treetaggerwidget
+Processing dependencies for MyWidgets==0.0.0
+Finished processing dependencies for MyWidgets==0.0.0
+
+C:\Users\mwuethr2\Desktop\TreetaggerWidget\TreetaggerWidget>cd C:\Users\mwuethr2
+\Desktop\TreetaggerWidget\TreetaggerWidget\MyOrangeWidgets
+
+C:\Users\mwuethr2\Desktop\TreetaggerWidget\TreetaggerWidget\MyOrangeWidgets>pyth
+on OWInteger.py
+
+C:\Users\mwuethr2\Desktop\TreetaggerWidget\TreetaggerWidget\MyOrangeWidgets>pyth
+on C:\Users\mwuethr2\Desktop\TreetaggerWidget\TreetaggerWidget\MyOrangeWidgets\O
+WTreetagger.py
+
+C:\Users\mwuethr2\Desktop\TreetaggerWidget\TreetaggerWidget\MyOrangeWidgets>
