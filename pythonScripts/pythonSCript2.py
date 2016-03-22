525c0d26bf7c265383e88b2ee2a563bc8dd05980
xbarros <xav.ba@hotmail.com>
2016-03-22 22:58:36 +0100

    Update pythonSCript2.py

--- pythonScripts/pythonSCript2.py
+++ pythonScripts/pythonSCript2.py
@@ -9,15 +9,16 @@
 import Orange
 from OWWidget import *
 import OWGUI
-
+from _textable.widgets.LTTL.Segmenter import Segmenter
+from _textable.widgets.LTTL.Segmentation import Segmentation
 
 class OWTreetagger(OWWidget):
     """Orange widget for adding an integer value to the input"""
     
     # Widget settings declaration...
     settingsList = [
-        'integer',
-        'operation'
+        'TextInput',
+        'TreetaggerLink',
     ]   
     
     def __init__(self, parent=None, signalManager=None):
@@ -27,62 +28,79 @@ class OWTreetagger(OWWidget):
 
         #----------------------------------------------------------------------
         # Channel definitions...
-        self.inputs = [('Intenger', str, self.processInputData)]     
+        self.inputs = [('TextInput', Segmentation, self.processInputData)]     
         self.outputs = [('Intenger', str)]
 
         #----------------------------------------------------------------------
         # Settings and other attribute initializations...
-        self.integer = ""  
-        self.operation = 'addition'
+        self.TextInput = ""  
+        self.TreetaggerLink = Segmenter()
         self.loadSettings()
         
         self.inputData = None   # NB: not a setting.
         
         #----------------------------------------------------------------------
         # User interface...
-        
-        """self.optionsBox = OWGUI.widgetBox(self.controlArea, 'Options')
-        OWGUI.spin(
-            widget=self.optionsBox,          
+       
+        # infoBox1 sert pour trouver le lien vers Treetagger mais manque fonction
+        self.infoBox1 = OWGUI.widgetBox(self.controlArea, u"Chemin d'acces vers Treetagger: ", addSpace=True)
+        OWGUI.lineEdit(
+            widget = self.infoBox1,
+            master = self,
+            value = '',
+            label = 'copier le lien ici: ',
+            tooltip='on rentre le chemin pour retrouver Treetagger.',
+            #callback=self.verifier_treetagger()
+        )
+        OWGUI.button(
+            widget=self.infoBox1, 
             master=self, 
-            value='integer',
-            label='Value:', 
-            callback=self.sendData,
-            tooltip='Select an integer value to add to input.',
-            min=0, 
-            max=10, 
-            step=1,
+            label='Enregistrer', 
+            addToLayout=False, 
+            default=True,
+            #callback = self.valider(),
         )
-        
-        self.optionsBox.setDisabled(True) # --> desactive la box   /!\
-        
-        self.operationBox = OWGUI.widgetBox(self.controlArea, 'Operation')
-        OWGUI.comboBox(
-            widget = self.operationBox, 
-            master = self, 
-            value = "operation",
-            items = ['addition', 'soustraction', 'multiplication'],
-            callback=self.sendData,
-            sendSelectedValue = True
-        )
+        #----------------------------------------------------------------------
 
-        """
+        # infoBox2 noter le nom de l'etiquette
+        self.infoBox2 = OWGUI.widgetBox(self.controlArea, u"Option: ")
+        OWGUI.lineEdit(
+            widget = self.infoBox2,
+            master = self,
+            value = '',
+            label = 'Output segment label: ',
+            tooltip="on donne l'identifiant qu'auront chaque segment",
+            #callback=self.
+        )
+        #-----------------------------------------------------------------------
+
+        # infoBox3 donne des info sur input et output
+        self.infoBox3 = OWGUI.widgetBox(self.controlArea, u"Option: ")
         
-        infoBox = OWGUI.widgetBox(self.controlArea, u'Info')
         self.infoLine = OWGUI.widgetLabel( # NB: using self here enables us to
                                            # access the label in other methods.
-            widget=infoBox,              
+            widget=self.infoBox3,              
             label='No input.',
         )
-
-        self.resize(10, 10)
-
-        self.sendData()
+        #------------------------------------------------------------------------
         
+    def verifier_treetagger():
+        #qqn doit faire 
+        #voir dans si le dossier contient un dossier bin ou lin si oui ok sinon pas ok
+        pass
+        
+    def valider():
+        #bla
+        pass
+    #----------------------------------------------------------------------------
+    # def ok :
+    
+    #recoit l'input
     def processInputData(self, inputData):
         """Method that processes the input data (as specified in __init__)."""
         # Store input data in attribute of this widget (so it can be accessed
         # from other methods).
+     
         self.inputData = inputData  
         
         """
@@ -96,27 +114,27 @@ class OWTreetagger(OWWidget):
         # Send data to output.
         self.sendData()
         
+        
     def sendData(self):
-        """Compute result of widget processing and send to output"""
+        #Compute result of widget processing and send to output
         # Important: if input data is None, propagate this value to output...
         if self.inputData is None:
             self.infoLine.setText('No input.')
-            self.send('Intenger', None)
+            self.send('TreetaggerLink', None)
             
         else:
             result = self.inputData 
             self.infoLine.setText(
-                '%s' % (self.inputData)  # utiliser ca par la suite !!!     -->  , self.integer, result)
+                'Il y a %i segment en input ' %len((self.inputData))
             )
-            self.send('Intenger', result)
+            self.send('Treetagger', result)
             
-            
-
 
 if __name__=='__main__':
     myApplication = QApplication(sys.argv)
     myWidget = OWTreetagger()
     myWidget.show()
-    myWidget.processInputData()
+    #segmenter = Segmenter()
+    myWidget.processInputData(u"How are you?")
     myApplication.exec_()
     
